6bf72175a5b4f669836bc3174fc6658e5d39ae57
Nick T <nick0207022@gmail.com>
2020-02-04 14:48:09 -0800

    Added new flags, removed old ones

--- alarmclock.asm
+++ alarmclock.asm
@@ -83,12 +83,15 @@ Mode_sel:     ds 1 ;
 ; In the 8051 we have variables that are 1-bit in size.  We can use the setb, clr, jb, and jnb
 ; instructions with these variables.  This is how you define a 1-bit variable:
 bseg
-seconds_flag:      dbit 1 ; Set to one in the ISR every time 1000 ms had passed 
-AM_flag:           dbit 1 ;
-AM_flag2:          dbit 1 ;
-AM_flag3:          dbit 1 ;
-Alarm_flag:        dbit 1 ;
-Clock_pause:       dbit 1 ;
+error_flag:        dbit 1 ; Set to one in the ISR every time 1000 ms had passed 
+speak_flag:        dbit 1 ;
+activation_flag:   dbit 1 ;
+soaking_flag:      dbit 1 ;
+rampUp_flag:       dbit 1 ;
+reflow_flag:       dbit 1 ;
+coolDown_flag:     dbit 1 ;
+finished_flag:     dbit 1 ;
+
 
 cseg
 ; These 'equ' must match the wiring between the microcontroller and the LCD!
